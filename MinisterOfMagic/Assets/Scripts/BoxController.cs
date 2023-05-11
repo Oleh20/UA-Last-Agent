3c534e818d9d5b60de5659a9938f6d9064b8b708
Rysslann <Ryslan10836@gmail.com>
2023-05-11 21:26:19 +0300

    creativ logik mission

---
+++ MinisterOfMagic/Assets/Scripts/BoxController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxController : MonoBehaviour
+{
+
+    public float moveSpeed = 2f;
+    public bool isMissionStarted = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    
+
+}
+
---
+++ MinisterOfMagic/Assets/Scripts/MIsion/MIsison Krim/MissionKrim.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissionKrim : Mission
+{
+
+    public override void LogicMission() 
+    {
+       
+    }
+}
